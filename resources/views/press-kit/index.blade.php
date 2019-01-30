6032b89e1033e90d1520a2571dcab1a5fcb8adaa
Mochammad Rezza Wikandito <reza.wikan.dito@gmail.com>
2019-01-30 11:34:24 +0800

    some updates v.1.5

--- resources/views/press-kit/index.blade.php
+++ resources/views/press-kit/index.blade.php
@@ -15,39 +15,74 @@
     <div class="row">
         <div class="col-md-12">
             <h3 class="text-center">Foto</h3>
-            <p>Segera</p>
-            {{-- <div class="row">
-              <div class="col-md-6 col-sm-6 col-lg-4">
+            {{-- <p>Segera</p> --}}
+            <div class="card-columns">
                 <div class="card">
-                  <a href="{{ asset('storage/img/photo/1.jpeg') }}" data-fancybox="images" data-caption="Backpackers following a dirt trail">
-                    <img src="{{ asset('storage/img/photo/1.jpeg') }}" />
-                    <div class="card-body">
-                      <p class="card-text"><small class="text-muted">Pulau Plastik host Gede Robi Supriyanto di TPA Suwung, Denpasar (Photo credit: Asa Film)</small></p>
-                    </div>
+                  <a href="{{ asset('storage/img/photo/1.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/1.jpg') }}" />
                   </a>
                 </div>
-              </div>
-              <div class="col-md-6 col-sm-6 col-lg-4">
                 <div class="card">
-                  <a href="{{ asset('storage/img/photo/2.jpg') }}" data-fancybox="images" data-caption="Backpackers following a dirt trail">
+                  <a href="{{ asset('storage/img/photo/2.jpg') }}" data-fancybox="images" data-caption="">
                     <img src="{{ asset('storage/img/photo/2.jpg') }}" />
-                    <div class="card-body">
-                      <p class="card-text"><small class="text-muted">Pulau Plastik host Gede Robi Supriyanto memilah sampah plastik yang ditemukan di tepi jalan di Gianyar  (Photo credit: Asa Film)</small></p>
-                    </div>
                   </a>
                 </div>
-              </div>
-              <div class="col-md-6 col-sm-6 col-lg-4">
                 <div class="card">
-                  <a href="{{ asset('storage/img/photo/3.jpg') }}" data-fancybox="images" data-caption="Backpackers following a dirt trail">
+                  <a href="{{ asset('storage/img/photo/3.jpg') }}" data-fancybox="images" data-caption="">
                     <img src="{{ asset('storage/img/photo/3.jpg') }}" />
-                    <div class="card-body">
-                      <p class="card-text"><small class="text-muted">Pulau Plastik host Gede Robi Supriyanto berdialog dengan warga setempat yang terkena dampak sampah plastik (Photo credit: Andre Dananjaya/Kopernik)</small></p>
-                    </div>
                   </a>
                 </div>
-              </div>
-            </div> --}}
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/4.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/4.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/5.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/5.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/6.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/6.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/7.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/7.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/8.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/8.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/9.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/9.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/10.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/10.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/11.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/11.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/12.jpg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/12.jpg') }}" />
+                  </a>
+                </div>
+                <div class="card">
+                  <a href="{{ asset('storage/img/photo/13.jpeg') }}" data-fancybox="images" data-caption="">
+                    <img src="{{ asset('storage/img/photo/13.jpeg') }}" />
+                  </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
