7dca172ecb81e6e562f9c68d0c799476a214dd4d
Mochammad Rezza Wikandito <reza.wikan.dito@gmail.com>
2019-03-15 17:08:36 +0800

    remove dropdown in resources

--- resources/views/navigation/navigation-default.blade.php
+++ resources/views/navigation/navigation-default.blade.php
@@ -38,13 +38,13 @@
         </div>
       </li>
       <li class="nav-item dropdown ml-1 mr-3">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link" href="{{route('resources.index')}}">
         Resources</a>
-        <div class="dropdown-menu fade" aria-labelledby="navbarDropdown">
+        {{-- <div class="dropdown-menu fade" aria-labelledby="navbarDropdown">
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="{{route('resources.index')}}#kenapa-plastik-menjadi-masalah">Kenapa plastik sebuah masalah?</a>
           <a class="dropdown-item" href="{{route('resources.index')}}#apa-yang-bisa-saya-lakukan">Apa yang bisa saya lakukan?</a>
-        </div>
+        </div> --}}
       </li>
       <li class="nav-item ml-1 mr-3">
         <a class="nav-link" href="{{ route('contact-us.index')}}">Contact Us</a>
